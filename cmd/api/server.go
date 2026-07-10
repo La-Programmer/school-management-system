5b0394b876222a6d6fe29f5a21b8e32054168b0d
LaProgrammer <justinoghenekomeebedi@gmail.com>
2026-07-10 13:59:56 +0100

    Project setup up till DB integration level

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,100 +1,58 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
+	"os"
+	"time"
+
+	mw "rest-api/internal/api/middlewares"
+	"rest-api/internal/api/router"
+	"rest-api/internal/repository/sqlconnect"
+	"rest-api/pkg/utils"
+
+	"github.com/joho/godotenv"
 )
 
-type User struct {
-	Name string `json:"name"`
-	Age  string `json:"age"`
-	City string `json:"city"`
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello root route"))
-}
-
-func teachersHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		fmt.Println(r.URL.Path)
-		path := strings.TrimPrefix(r.URL.Path, "/teachers/")
-		userID := strings.TrimSuffix(path, "/")
-
-		fmt.Println("The ID is:", userID)
-
-		fmt.Println("Query Params:", r.URL.Query())
-
-		queryParams := r.URL.Query()
-		sortBy := queryParams.Get("sortBy")
-		key := queryParams.Get("key")
-		sortOrder := queryParams.Get("sortOrder")
-
-		fmt.Printf("Sort By: %v, Sort Order: %v, Key: %v\n", sortBy, sortOrder, key)
-
-		w.Write([]byte("Hello GET method on teachers route"))
-	case http.MethodPost:
-		w.Write([]byte("Hello POST method on teachers route"))
-	case http.MethodPut:
-		w.Write([]byte("Hello PUT method on teachers route"))
-	case http.MethodPatch:
-		w.Write([]byte("Hello PATCH method on teachers route"))
-	case http.MethodDelete:
-		w.Write([]byte("Hello DELETE method on teachers route"))
-	}
-}
-
-func studentsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		w.Write([]byte("Hello GET method on students route"))
-	case http.MethodPost:
-		w.Write([]byte("Hello POST method on students route"))
-	case http.MethodPut:
-		w.Write([]byte("Hello PUT method on students route"))
-	case http.MethodPatch:
-		w.Write([]byte("Hello PATCH method on students route"))
-	case http.MethodDelete:
-		w.Write([]byte("Hello DELETE method on students route"))
-	default:
-		w.Write([]byte("Hello on students route"))
-	}
-}
-
-func execsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		w.Write([]byte("Hello GET method on execs route"))
-	case http.MethodPost:
-		w.Write([]byte("Hello POST method on execs route"))
-	case http.MethodPut:
-		w.Write([]byte("Hello PUT method on execs route"))
-	case http.MethodPatch:
-		w.Write([]byte("Hello PATCH method on execs route"))
-	case http.MethodDelete:
-		w.Write([]byte("Hello DELETE method on execs route"))
-	default:
-		w.Write([]byte("Hello on execs route"))
-	}
-}
-
 func main() {
+	godotenv.Load("../../.env")
+
+	dbName := os.Getenv("DB_NAME")
+	_, err := sqlconnect.ConnectDb(dbName)
+	if err != nil {
+		fmt.Println("Error--------:", err)
+		return
+	}
 	port := ":3000"
 
-	http.HandleFunc("/", rootHandler)
+	cert := "cert.pem"
+	key := "key.pem"
 
-	http.HandleFunc("/teachers/", teachersHandler)
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
 
-	http.HandleFunc("/students", studentsHandler)
+	rl := mw.NewRateLimiter(5, time.Minute)
+	hppOptions := mw.HppOptions{
+		CheckQuery:                  true,
+		CheckBody:                   true,
+		CheckBodyOnlyForContentType: "application/x-www-form-urlencoded",
+		Whitelist:                   []string{"allowedParam", "sortBy", "sortOrder", "name", "country", "age", "class"},
+	}
+	secureMux := utils.ApplyMiddlewares(router.Router(), mw.Hpp(hppOptions), mw.Compression, mw.SecurityHeaders, mw.ResponseTimeMiddleware, rl.Middleware, mw.Cors)
+	// secureMux := mw.SecurityHeaders(mux)
 
-	http.HandleFunc("/execs", execsHandler)
+	server := &http.Server{
+		Addr:      port,
+		Handler:   secureMux,
+		TLSConfig: tlsConfig,
+	}
 
 	fmt.Println("Server is running on port", port)
 
-	err := http.ListenAndServe(port, nil)
+	err = server.ListenAndServeTLS(cert, key)
 	if err != nil {
 		log.Fatalln("Error starting the server: ", err)
 	}
